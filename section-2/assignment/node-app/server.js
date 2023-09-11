6f339116e0e24457e586e5ac8b8b9644a9bed5a4
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-09-11 17:10:12 +0300

    fix: image port mapping

--- section-2/assignment/node-app/server.js
+++ section-2/assignment/node-app/server.js
@@ -1,11 +1,13 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send(`
     <h1>Hello from inside the very basic Node app!</h1>
   `);
-})
+});
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
